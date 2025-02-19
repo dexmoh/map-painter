072ee40d0635e9d2fdf3b5ae267e7a959f17a18f
dexmoh <lazar.petrovic2310@gmail.com>
2025-02-19 17:06:17 +0100

    better compression

--- scripts/compress_geodata.py
+++ scripts/compress_geodata.py
@@ -1,11 +1,29 @@
 import json
-
-# Number of vertices the program will 'skip' over,
-# higher number results in more compression and less detail.
-compression_level = 3
+import math
 
 in_file = "provinces.geo.json"
 out_file = "provinces_compressed.geo.json"
+
+# Calculate a distance between two points on Earth.
+def haversine(lat1, lon1, lat2, lon2):
+    # Radius of the Earth in kilometers
+    R = 6371.0
+    
+    # Convert latitude and longitude from degrees to radians
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+    
+    # Differences in coordinates
+    dlat = lat2_rad - lat1_rad
+    dlon = lon2_rad - lon1_rad
+    
+    # Haversine formula
+    a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    
+    return R * c # km
 
 def main():
     data = {}
@@ -22,7 +40,19 @@ def main():
     for i, feature in enumerate(data["features"]):
         for j, polygons in enumerate(feature["geometry"]["coordinates"]):
             for k, points in enumerate(polygons):
-                compressed_data["features"][i]["geometry"]["coordinates"][j][k] = points[::compression_level]
+                if len(points) < 50:
+                    continue
+
+                new_points = []
+                for l, point in enumerate(points):
+                    if (l == 0) or ((l + 1) >= len(points)):
+                        new_points.append(point)
+                        continue
+
+                    if haversine(point[0], point[1], points[l + 1][0], points[l + 1][1]) > 1.0:
+                        new_points.append(point)
+
+                compressed_data["features"][i]["geometry"]["coordinates"][j][k] = new_points
     
     with open(out_file, "w", encoding="utf-8") as f:
         json.dump(compressed_data, f)
